7785e2017e643f1a19053d6dae46d7ffc195d355
Fernando Cortez <fernandocortez@users.noreply.github.com>
2022-06-28 17:57:39 +0000

    chore: Upgraded dev dependencies and config

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@
 // afect the deployment and dowload
 
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import css from "rollup-plugin-css-only";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
@@ -20,19 +21,15 @@ export default {
     svelte({
       // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write("public/bundle.css");
-      }
     }),
+    css({output: "public/bundle.css"}),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration —
     // consult the documentation for details:
     // https://github.com/rollup/rollup-plugin-commonjs
-    resolve(),
+    nodeResolve(),
     commonjs(),
 
     // If we're building for production (npm run build
